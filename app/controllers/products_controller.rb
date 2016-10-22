19a84a0d18392ecc060184cd339ef01ea1239f5d
Jara2392 <daniel.jaramillog@udea.edu.co>
2016-10-22 11:04:10 -0500

    CRUD completo

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,24 +7,41 @@ class ProductsController < ApplicationController
       @product = Product.new(product_params)
       if @product.save
         flash[:notice] = 'The product have been saved'
-        redirect_to @product
+        redirect_to product_path(id: @product.id, it_was: 'created')
       else
         render :new
       end
   end
 
   def edit
+    @product = Product.find(params[:id])
+  end
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      flash[:notice] = 'Store was siccessfully updated'
+      redirect_to product_path(id: @product.id, it_was: 'updated')
+    else
+      render edit
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    flash[:notice] = 'Product was successfully deleted.'
+    redirect_to products_path
   end
 
   def index
     @products = Product.all
   end
 
-  def destroy
-  end
+
 
   def show
     @product = Product.find(params[:id])
+    @action = params[:it_was]
   end
 
   private
