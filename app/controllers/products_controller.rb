af4f9a6fe71e1aa0d8f7a3530778f27320fb74e1
Jara2392 <daniel.jaramillog@udea.edu.co>
2016-10-22 10:02:42 -0500

    Adding validations

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,15 +3,32 @@ class ProductsController < ApplicationController
     @product = Product.new
   end
 
+  def create
+      @product = Product.new(product_params)
+      if @product.save
+        flash[:notice] = 'The product have been saved'
+        redirect_to @product
+      else
+        render :new
+      end
+  end
+
   def edit
   end
 
   def index
+    @products = Product.all
   end
 
   def destroy
   end
 
   def show
+    @product = Product.find(params[:id])
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:name,:reference,:price,:quantity,:brand,:description)
   end
 end
