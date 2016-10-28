e4add557e6d069214e34977d8ef3810c26f6f8dd
Jara2392 <daniel.jaramillog@udea.edu.co>
2016-10-27 19:53:31 -0500

    Restrigiendo acceso de clientes

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,8 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!
   before_action :find_product, only: [:edit,:show,:update,:destroy]
+  before_action :its_admin?
+
 
   def new
     @product = Product.new
@@ -50,6 +52,12 @@ class ProductsController < ApplicationController
 
   def find_product
     @product = Product.find(params[:id])
+  end
+
+  def its_admin?
+    unless current_user.admin?
+      redirect_to root_path, :alert => "Acceso denegado, no posee permisos como administrador"
+    end
   end
 
 
