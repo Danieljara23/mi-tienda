cf7fd4900265322e662e466f2d623dae20365764
Jara2392 <daniel.jaramillog@udea.edu.co>
2016-11-17 20:47:39 -0500

    Mailer on production-fix3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -100,7 +100,7 @@ Rails.application.configure do
   ActionMailer::Base.smtp_settings = {
      :user_name => ENV['SENDGRID_USERNAME'],
      :password => ENV['SENDGRID_PASSWORD'],
-     :domain => 'https://latiendadememo.herokuapp.com/',
+     :domain => 'https://tienda-de-daniel.herokuapp.com/',
      :address => 'smpt.sendgrid.net',
      :port => 587,
      :authentication => :plain,
